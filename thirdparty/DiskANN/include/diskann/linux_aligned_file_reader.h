8a169e6e5fd2d1113cd522821e23ba9614f7f3a0
pastelkormad <roblexa948@gmail.com>
2024-11-23 09:16:57 +0000

    reader modified T1816

--- thirdparty/DiskANN/include/diskann/linux_aligned_file_reader.h
+++ thirdparty/DiskANN/include/diskann/linux_aligned_file_reader.h
@@ -6,12 +6,96 @@
 #include "aligned_file_reader.h"
 #include "aio_context_pool.h"
 
+// PASTEL: custom batch modified
+#include <liburing.h>
+#include <linux/nvme_ioctl.h>
+#include <algorithm>
+#include <linux/fs.h>
+#include <linux/fiemap.h>
+#include <sys/ioctl.h>
+#undef BLOCK_SIZE // defined in linux/fs.h, causes conflicts in PQFlashIndex
+// end
+
+struct Extent {
+    uint64_t logical_start;
+    uint64_t physical_start;
+    uint64_t length;
+};
+
 class LinuxAlignedFileReader : public AlignedFileReader {
- private:
+private:
   uint64_t     file_sz;
   FileHandle   file_desc;
   io_context_t bad_ctx = (io_context_t) -1;
   std::shared_ptr<AioContextPool> ctx_pool_;
+
+  FileHandle   disk_fd;
+  uint64_t     partition_start;
+  uint64_t     logical_block_size;
+  
+  struct io_uring ring;
+  
+  // filefrag extents to tree and search
+    class IntervalTree {
+    public:
+      IntervalTree(const std::vector<Extent>& extents) { root = buildIntervalTree(extents, 0, extents.size() - 1); }
+      ~IntervalTree() { deleteIntervalTree(root); }
+          // search for the physical offset of the logical offset
+          // returns LBA *within partition* (not in the entire disk) in bytes
+      uint64_t search(uint64_t logical_off) const { return searchIntervalTree(root, logical_off); };
+    private:
+      class IntervalNode {
+        public:
+          IntervalNode(const Extent& e) : extent(e), max(e.logical_start + e.length - 1), left(0), right(0) {}
+          Extent extent;
+          uint64_t max; // maximum logical_end in this subtree
+          IntervalNode *left;
+          IntervalNode *right;
+        }; // class IntervalNode (nested)
+
+        IntervalNode* buildIntervalTree(const std::vector<Extent>& sorted_extents, int start, int end){
+          if (start > end) {
+            return 0;
+          }
+
+          int mid = start + (end - start) / 2;
+          IntervalNode* node = new IntervalNode(sorted_extents[mid]);
+          node->left = buildIntervalTree(sorted_extents, start, mid - 1);
+          node->right = buildIntervalTree(sorted_extents, mid + 1, end);
+
+          // Update the max value
+          uint64_t left_max = node->left ? node->left->max : 0;
+          uint64_t right_max = node->right ? node->right->max : 0;
+          node->max = std::max({node->extent.logical_start + node->extent.length - 1, left_max, right_max});
+
+          return node;
+        }; // buildIntervalTree
+
+        void deleteIntervalTree(IntervalNode* node){
+          if (!node) return;
+          deleteIntervalTree(node->left);
+          deleteIntervalTree(node->right);
+          delete node;
+        }; // deleteIntervalTree
+
+        uint64_t searchIntervalTree (const IntervalNode* node, uint64_t logical_off) const {
+          if (!node) {
+            return -1;
+          }
+          if (node->extent.logical_start <= logical_off && logical_off < node->extent.logical_start + node->extent.length) {
+            return node->extent.physical_start + (logical_off - node->extent.logical_start);
+          }
+
+          if (node->left && logical_off <= node->left->max) {
+            return searchIntervalTree(node->left, logical_off);
+          } else {
+            return searchIntervalTree(node->right, logical_off);
+          }
+        }; // searchIntervalTree
+        IntervalNode* root;
+  };
+
+  IntervalTree* tree;
 
  public:
   LinuxAlignedFileReader();
--- thirdparty/DiskANN/src/linux_aligned_file_reader.cpp
+++ thirdparty/DiskANN/src/linux_aligned_file_reader.cpp
@@ -14,6 +14,9 @@
 
 namespace {
   static constexpr uint64_t n_retries = 10;
+  static constexpr uint64_t PAGE_SIZE = 4096;
+  static constexpr uint64_t pages_per_cmd = 8;
+  static constexpr uint64_t maxnr_cap = 128;
 
   typedef struct io_event io_event_t;
   typedef struct iocb     iocb_t;
@@ -118,6 +121,341 @@ namespace {
       */
     }
   }
+
+
+  // PASTEL: custom batch modified
+  
+struct io_uring_sqe_128 {
+	__u8	opcode;		/* type of operation for this sqe */
+	__u8	flags;		/* IOSQE_ flags */
+	__u16	ioprio;		/* ioprio for the request */
+	__s32	fd;		/* file descriptor to do IO on */
+	union {
+		__u64	off;	/* offset into file */
+		__u64	addr2;
+		struct {
+			__u32	cmd_op;
+			__u32	__pad1;
+		};
+	};
+	union {
+		__u64	addr;	/* pointer to buffer or iovecs */
+		__u64	splice_off_in;
+	};
+	__u32	len;		/* buffer size or number of iovecs */
+	union {
+		__kernel_rwf_t	rw_flags;
+		__u32		fsync_flags;
+		__u16		poll_events;	/* compatibility */
+		__u32		poll32_events;	/* word-reversed for BE */
+		__u32		sync_range_flags;
+		__u32		msg_flags;
+		__u32		timeout_flags;
+		__u32		accept_flags;
+		__u32		cancel_flags;
+		__u32		open_flags;
+		__u32		statx_flags;
+		__u32		fadvise_advice;
+		__u32		splice_flags;
+		__u32		rename_flags;
+		__u32		unlink_flags;
+		__u32		hardlink_flags;
+		__u32		xattr_flags;
+		__u32		msg_ring_flags;
+		__u32		uring_cmd_flags;
+	};
+	__u64	user_data;	/* data to be passed back at completion time */
+	/* pack this to avoid bogus arm OABI complaints */
+	union {
+		/* index into fixed buffers, if used */
+		__u16	buf_index;
+		/* for grouped buffer selection */
+		__u16	buf_group;
+	} __attribute__((packed));
+	/* personality to use, if used */
+	__u16	personality;
+	union {
+		__s32	splice_fd_in;
+		__u32	file_index;
+		struct {
+			__u16	addr_len;
+			__u16	__pad3[1];
+		};
+	};
+	union {
+		struct {
+			__u64	addr3;
+			__u64	__pad2[1];
+		};
+		/*
+		 * If the ring is initialized with IORING_SETUP_SQE128, then
+		 * this field is used for 80 bytes of arbitrary command data
+		 */
+		nvme_uring_cmd cmd;
+	};
+    __u8 __pad4[8];
+};
+
+// only merge pages when buffers are adjacent in memory space
+// otherwise, create a normal read command
+int cmd_assign_pgs(struct nvme_uring_cmd *cmd, const struct AlignedRead* reads,
+int count, unsigned int logical_block_size = 512){
+    if (count <= 0 || count > 8) {
+        std::stringstream err;
+        err << "cmd_assign_pgs: count should be in [1, 8]";
+        throw diskann::ANNException(err.str(), -1, __FUNCSIG__, __FILE__, __LINE__);
+    }
+
+    cmd->nsid = 1;
+	static constexpr unsigned int unused_mark = 0xffffffff; // 0xffffffff is used as a flag for unused
+	int can_be_batched_up_to = 0; // end inclusive
+	for(int i=1; i<count; i++){
+		if(((char*)(reads[i].buf) == (char*)(reads[i-1].buf) + reads[i-1].len) &&
+		(reads[i].len == PAGE_SIZE))
+			can_be_batched_up_to++;
+		else break;
+	}
+	
+    cmd->opcode = (can_be_batched_up_to == 0) ? 2 : 144; // read/write or custom batch command
+    cmd->addr = (unsigned long long)(reads[0].buf);
+    cmd->data_len = (can_be_batched_up_to == 0) ? reads[0].len : PAGE_SIZE * (can_be_batched_up_to + 1);
+
+	if(can_be_batched_up_to == 0){
+    	*((unsigned long long*)(&(cmd->cdw10))) = (reads[0].offset) / logical_block_size;
+		cmd->cdw12 = ((reads[0].len / logical_block_size) - 1) & 0xFFFF;
+		return 1;
+	}
+    // cdws for custom batch command (opcode 144) is not contiguous
+	// logical_block_size does not matter for custom batch command
+    for(int i=0; i<2; i++){
+        *((unsigned int*)(&(cmd->cdw2)) + i) = (i <= can_be_batched_up_to) ? (reads[i].offset / PAGE_SIZE) : unused_mark;
+    }
+    // as i starts from 2, base address is 8 bytes ahead of cdw10 (actual base address) 
+    for(int i=2; i<8; i++){
+        *((unsigned int*)(&(cmd->metadata_len)) + i) = (i <= can_be_batched_up_to) ? (reads[i].offset / PAGE_SIZE) : unused_mark;
+    }
+
+	return can_be_batched_up_to + 1;
+}
+
+// Get the starting offset of the partition in which the file resides, in blocks, and size of logical blocks
+int GetPartitionOffset(const int fd, uint64_t* ret_off, uint64_t* ret_logical_block_size) {
+    if(!ret_off) return 1;
+    struct stat st;
+    if (fstat(fd, &st) < 0) {
+        std::stringstream err;
+        err << "Error getting file stats: " << strerror(errno);
+        throw diskann::ANNException(err.str(), -1, __FUNCSIG__, __FILE__, __LINE__);
+    }
+    dev_t dev = st.st_dev;
+    dev_t major = dev >> 8;
+    dev_t minor = dev & 255UL;
+    std::string devpath = "/sys/dev/block/" + std::to_string(major) + ":" + std::to_string(minor);
+    // if devpath/partition exists, then it is a partition of a disk. Otherwise, it is a whole disk.
+    std::string partition = devpath + "/partition";
+    if(access(partition.c_str(), F_OK)) {
+		// access returns nonzero if partition file does not exist, i.e. dev is a whole disk
+        *ret_off = 0;
+		std::string logical_block_size_path = devpath + "/queue/logical_block_size";
+		std::ifstream logical_block_size_file(logical_block_size_path);
+		if(!logical_block_size_file.is_open()){
+      std::stringstream err;
+      err << "Error opening file: " << strerror(errno);
+      throw diskann::ANNException(err.str(), -1, __FUNCSIG__, __FILE__, __LINE__);
+		}
+		logical_block_size_file >> *ret_logical_block_size;
+        return 0;
+    }
+    // get the partition offset: in devpath/start
+    // partition offset is in units specified in devpath/../queue/logical_block_size (usually 512 bytes)
+    else{
+        std::string offset_path = devpath + "/start";
+        std::ifstream offset_file(offset_path);
+        uint64_t logical_block_size;
+        std::string logical_block_size_path = devpath + "/../queue/logical_block_size";
+        if(!offset_file.is_open()){
+          std::stringstream err;
+          err << "Error opening file: " << strerror(errno);
+          throw diskann::ANNException(err.str(), -1, __FUNCSIG__, __FILE__, __LINE__);
+        }
+        offset_file >> *ret_off;
+
+        std::ifstream logical_block_size_file(logical_block_size_path);
+        if(!logical_block_size_file.is_open()){
+          std::stringstream err;
+          err << "Error opening file: " << strerror(errno);
+          throw diskann::ANNException(err.str(), -1, __FUNCSIG__, __FILE__, __LINE__);
+        }
+        logical_block_size_file >> *ret_logical_block_size;
+    }
+    return 0;
+}
+
+// Get the starting offset of the partition in which the file resides, in blocks, and size of logical blocks
+int GetPartitionOffset(const std::string& filename, uint64_t* ret_off, uint64_t* ret_logical_block_size) {
+	int fd = open(filename.c_str(), O_RDONLY);
+	if (fd < 0) {
+    std::stringstream err;
+    err << "Error opening file: " << strerror(errno);
+    throw diskann::ANNException(err.str(), -1, __FUNCSIG__, __FILE__, __LINE__);
+	}
+	int ret = GetPartitionOffset(fd, ret_off, ret_logical_block_size);
+	close(fd);
+	return ret;
+}
+
+int getFileExtents(const int fd, std::vector<Extent>* extentList){
+    struct fiemap *fiemap;
+    struct fiemap_extent *extents;
+
+    // first call to only get the number of extents
+    fiemap = static_cast<struct fiemap *>(calloc(1, sizeof(struct fiemap)));
+    fiemap->fm_start = 0;
+    fiemap->fm_length = FIEMAP_MAX_OFFSET;
+    fiemap->fm_flags = FIEMAP_FLAG_SYNC;
+    fiemap->fm_extent_count = 0;
+
+    if (ioctl(fd, FS_IOC_FIEMAP, fiemap) < 0) {
+        std::stringstream err;
+        err << "Error in FIEMAP ioctl: " << strerror(errno);
+        free(fiemap);
+        throw diskann::ANNException(err.str(), -1, __FUNCSIG__, __FILE__, __LINE__);
+    }
+
+    unsigned int num_extents = fiemap->fm_mapped_extents;
+
+    free(fiemap);
+    // second call to get the extents, additionally allocate memory for the extents
+    fiemap = static_cast<struct fiemap *>(calloc(1, sizeof(struct fiemap) +
+                    num_extents * sizeof(struct fiemap_extent)));
+    fiemap->fm_start = 0;
+    fiemap->fm_length = FIEMAP_MAX_OFFSET;
+    fiemap->fm_flags = FIEMAP_FLAG_SYNC;
+    fiemap->fm_extent_count = num_extents;
+    lseek64(fd, 0, SEEK_SET);
+    if (ioctl(fd, FS_IOC_FIEMAP, fiemap) < 0){
+        std::stringstream err;
+        err << "Error in FIEMAP ioctl: " << strerror(errno);
+        free(fiemap);
+        throw diskann::ANNException(err.str(), -1, __FUNCSIG__, __FILE__, __LINE__);
+    }
+
+    extents = &fiemap->fm_extents[0];
+    for (unsigned int i = 0; i < fiemap->fm_mapped_extents; i++) {
+        extentList->push_back({
+            extents[i].fe_logical,
+            extents[i].fe_physical,
+            extents[i].fe_length
+        });
+    }
+
+  free(fiemap);
+	return 0;
+}
+
+int getFileExtents(const std::string& filename, std::vector<Extent>* extentList) {
+    int fd = open(filename.c_str(), O_RDONLY);
+    if (fd < 0) {
+        std::stringstream err;
+        err << "Error opening file: " << strerror(errno);
+        throw diskann::ANNException(err.str(), -1, __FUNCSIG__, __FILE__, __LINE__);
+    }
+	int ret = getFileExtents(fd, extentList);
+    close(fd);
+    return 0;
+}
+
+// Open file descriptor of the disk containing the file.
+// Opens the whole disk by default, unless open_partition is set true, in which case it opens the partition.
+int open_disk_fd(int file_fd, bool open_partition = false, int oflag = O_RDONLY, bool generic = false){
+    struct stat st;
+    if (fstat(file_fd, &st) < 0) {
+        std::stringstream err; err << "Error getting file stats: " << strerror(errno);
+        throw diskann::ANNException(err.str(), -1, __FUNCSIG__, __FILE__, __LINE__);
+    }
+    dev_t dev = st.st_dev;
+    dev_t major = dev >> 8;
+    dev_t minor = dev & 255UL;
+    std::string devpath = "/dev/block/" + std::to_string(major) + ":" + std::to_string(minor);
+
+    // open partition if partition is true (do not check whether this is a partition)
+    int disk_fd;
+    if(open_partition)     disk_fd = open(devpath.c_str(), oflag);
+    // open disk if partition is false (should open the whole disk)
+    else{
+        std::string partition = "/sys" + devpath + "/partition";
+        int is_partition = !(access(partition.c_str(), F_OK)); // access returns 0 if partition exists.
+        if(is_partition){
+            // find parent disk name
+            // DEVNAME=nvmexnx in /sys/dev/block/major:minor/../uevent
+            std::string diskname_path = "/sys" + devpath + "/../uevent";
+            std::ifstream diskname_file(diskname_path);
+            std::string diskname;
+            if(!diskname_file.is_open()){
+                std::stringstream err; err << "Error getting file: " << strerror(errno);
+                throw diskann::ANNException(err.str(), -1, __FUNCSIG__, __FILE__, __LINE__);
+            }
+            std::string line;
+            while(std::getline(diskname_file, line)){
+                if(line.find("DEVNAME=") != std::string::npos){
+                    diskname = line.substr(strlen("DEVNAME="));
+                    break;
+                }
+            }
+			std::string full_diskname;
+			if(!generic) full_diskname = "/dev/" + diskname;
+			else{
+				// /dev/ng*n* instead of /dev/nvme*n*
+				if (diskname.substr(0, 4) == "nvme"){
+					full_diskname = "/dev/ng" + diskname.substr(4);
+				}
+				else {
+					std::stringstream err;
+          err << "Error: requested generic diskname, but the device name does not start with nvme";
+          throw diskann::ANNException(err.str(), -1, __FUNCSIG__, __FILE__, __LINE__);
+				}
+			}
+            disk_fd = open(full_diskname.c_str(), oflag);
+        }
+        // the partition is also the whole disk
+        else{
+			if (!generic) disk_fd = open(devpath.c_str(), oflag);
+			else{
+				std::string diskname_path = "/sys" + devpath + "/uevent";
+				std::ifstream diskname_file(diskname_path);
+				std::string diskname;
+				if(!diskname_file.is_open()){
+					std::stringstream err; err << "Error opening file: " << strerror(errno);
+          throw diskann::ANNException(err.str(), -1, __FUNCSIG__, __FILE__, __LINE__);
+				}
+				std::string line;
+				while(std::getline(diskname_file, line)){
+					if(line.find("DEVNAME=") != std::string::npos){
+						diskname = line.substr(strlen("DEVNAME="));
+						break;
+					}
+				}
+				// /dev/ng*n* instead of /dev/nvme*n*
+				if (diskname.substr(0, 4) == "nvme"){
+					std::string full_diskname = "/dev/ng" + diskname.substr(4);
+					disk_fd = open(full_diskname.c_str(), oflag);
+				}
+				else {
+					std::stringstream err;
+          err << "Error: requested generic diskname, but the device name does not start with nvme";
+          throw diskann::ANNException(err.str(), -1, __FUNCSIG__, __FILE__, __LINE__);
+				}
+			}
+        }
+    }
+    if(disk_fd < 0){
+        std::stringstream err;
+        err << "Error opening disk: " << strerror(errno);
+        throw diskann::ANNException(err.str(), -1, __FUNCSIG__, __FILE__, __LINE__);
+    }
+    return disk_fd;
+}
+
 }  // namespace
 
 LinuxAlignedFileReader::LinuxAlignedFileReader() {
@@ -149,6 +487,28 @@ void LinuxAlignedFileReader::open(const std::string &fname) {
   // error checks
   assert(this->file_desc != -1);
   LOG_KNOWHERE_DEBUG_ << "Opened file : " << fname;
+  this->disk_fd = open_disk_fd(this->file_desc, false, flags, true);
+  if (this->disk_fd < 0) {
+    std::stringstream err;
+    err << "Error opening disk: " << strerror(errno);
+    throw diskann::ANNException(err.str(), -1, __FUNCSIG__, __FILE__, __LINE__);
+  }
+  int err = GetPartitionOffset(this->disk_fd, &this->partition_start, &this->logical_block_size);
+  if (err != 0) {
+    std::stringstream err;
+    err << "Error getting partition offset: " << strerror(errno);
+    throw diskann::ANNException(err.str(), -1, __FUNCSIG__, __FILE__, __LINE__);
+  }
+  LOG_KNOWHERE_DEBUG_ << "Partition offset: " << this->partition_start << ", Logical block size: " << this->logical_block_size;
+
+  // get extents of the file
+  std::vector<Extent> extents;
+  getFileExtents(this->file_desc, &extents);
+  this->tree = new IntervalTree(extents);
+
+  // initialize ring
+  io_uring_queue_init(maxnr_cap, &this->ring, IORING_SETUP_SQE128 | IORING_SETUP_CQE32);
+
 }
 
 void LinuxAlignedFileReader::close() {
@@ -160,6 +520,11 @@ void LinuxAlignedFileReader::close() {
 
   ::close(this->file_desc);
   //  assert(ret != -1);
+
+  ::close(this->disk_fd);
+  
+  io_uring_queue_exit(&this->ring);
+  delete this->tree;
 }
 
 void LinuxAlignedFileReader::read(std::vector<AlignedRead> &read_reqs,
@@ -169,8 +534,80 @@ void LinuxAlignedFileReader::read(std::vector<AlignedRead> &read_reqs,
   }
   assert(this->file_desc != -1);
 
-  execute_io(ctx, this->ctx_pool_->max_events_per_ctx(), this->file_desc,
-             read_reqs);
+  // not using outside function because class members are used
+  // execute_io(ctx, this->ctx_pool_->max_events_per_ctx(), this->file_desc,
+  //            read_reqs);
+  const int queue_size = maxnr_cap;
+  const int maxnr = std::min(this->ctx_pool_->max_events_per_ctx(), maxnr_cap);
+  // the size of actual array in ring.sq.sqes is a power of 2
+  const int actual_queue_size = [](int q){int r=1; while(r<q) r<<=1; return r;}(queue_size);
+  const int actual_queue_mask = actual_queue_size - 1;
+  struct io_uring_sqe_128* sqes = (struct io_uring_sqe_128*)(ring.sq.sqes);
+  memset(sqes, 0, sizeof(struct io_uring_sqe_128) * actual_queue_size);
+
+	std::vector<AlignedRead> reqs_withdiskoffsets(read_reqs); // copy of read_reqs, convert to disk offsets
+
+	int sq_position;
+	int reqs_size = read_reqs.size();
+	int n_iters = (reqs_size + maxnr - 1) / maxnr;
+	int reqs_done = 0;
+
+	// for each command in read_reqs, convert LPN offset to disk offsets using tree
+	for(int i = 0; i < reqs_size; i++){
+		reqs_withdiskoffsets[i].offset = tree->search(read_reqs[i].offset) + partition_start * logical_block_size;
+	}
+
+	for (int iter=0; iter<n_iters; iter++){
+		int sq_position = ring.sq.sqe_tail;
+		int real_n_ops = std::min((int)maxnr, reqs_size - reqs_done);
+		int real_command_count = 0;
+		for(int i=0; i<real_n_ops;){
+			sqes[sq_position & actual_queue_mask].opcode = IORING_OP_URING_CMD;
+			sqes[sq_position & actual_queue_mask].cmd_op = NVME_URING_CMD_IO;
+			sqes[sq_position & actual_queue_mask].fd = disk_fd;
+			int pm = cmd_assign_pgs(&(sqes[sq_position & actual_queue_mask].cmd),  // command to assign
+			reqs_withdiskoffsets.data() + reqs_done + i, pages_per_cmd, logical_block_size);
+			// return value = how many reads there are in the command
+			if (pm == -1) {
+        std::stringstream err;
+        err << "cmd_assign_pgs: failed";
+        throw diskann::ANNException(err.str(), -1, __FUNCSIG__, __FILE__, __LINE__);
+				// fprintf(stderr, "cmd_assign_pgs: failed\n");
+				// return 1;
+			}
+			i += pm;
+			sq_position++;
+			real_command_count++;
+		}
+
+		*(ring.sq.ktail) += real_command_count;
+		ring.sq.sqe_tail += real_command_count;
+
+		int ret = io_uring_submit(&ring);
+		if(ret < 0){
+      std::stringstream err;
+      err << "io_uring_submit: " << strerror(-ret);
+      throw diskann::ANNException(err.str(), -1, __FUNCSIG__, __FILE__, __LINE__);
+			// fprintf(stderr, "io_uring_submit: %s\n", strerror(-ret));
+			// return 1;
+		}
+		struct io_uring_cqe *cqe;
+		ret = 0;
+		for(int i=0; i<real_command_count; i++){
+			ret=io_uring_wait_cqe(&ring, &cqe);
+			if (ret < 0) {
+        std::stringstream err;
+        err << "io_uring_wait_cqe: " << strerror(-ret);
+        throw diskann::ANNException(err.str(), -1, __FUNCSIG__, __FILE__, __LINE__);
+				// fprintf(stderr, "io_uring_wait_cqe: %s\n", strerror(-ret));
+				// return 1;
+			}
+			io_uring_cqe_seen(&ring, cqe);
+		}
+		reqs_done += real_n_ops;
+	}
+	return;
+
 }
 
 void LinuxAlignedFileReader::submit_req(io_context_t             &ctx,
